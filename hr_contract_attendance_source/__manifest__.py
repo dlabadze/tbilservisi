e25653bc65f5afb97858a559c27e11b2018d17e1
lizaKN <gvasalializa3@gmail.com>
2026-05-13 13:12:37 +0400

    change contract source to attendance

---
+++ hr_contract_attendance_source/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': 'HR Contract Attendance Source Action',
+    'version': '1.0',
+    'depends': ['hr_contract'],
+    'data': [
+        'views/hr_contract_views.xml',
+    ],
+    'installable': True,
+    'application': False,
+    'license': 'LGPL-3',
+}
