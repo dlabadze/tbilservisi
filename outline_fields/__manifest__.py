c2bc2c3133c792ca87edea382d71b3389151f141
lizaKN <gvasalializa3@gmail.com>
2026-05-22 13:28:20 +0400

    xazgasmit

---
+++ outline_fields/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'Form Fields Style',
+    'version': '18.0.1.0.0',
+    'category': 'Technical',
+    'depends': ['web'],
+    'assets': {
+        'web.assets_backend': [
+            'outline_fields/static/src/css/form_fields.css',
+        ],
+    },
+    'installable': True,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
