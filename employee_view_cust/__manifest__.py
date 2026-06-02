c593cc2746ed58fa6d5a0f85552f2611ba331701
lizaKN <gvasalializa3@gmail.com>
2026-06-02 12:34:36 +0400

    emp custom view

---
+++ employee_view_cust/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'Employee View Customization',
+    'version': '18.0.1.0.0',
+    'category': 'Human Resources',
+    'summary': 'Custom employee form view',
+    'depends': ['hr'],
+    'data': [
+        'views/employee_form_custom_view.xml',
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
