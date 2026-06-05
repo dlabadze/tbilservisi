17cefff6d4ba1f52bfcf3590557d8e13bbb0da1f
lizaKN <gvasalializa3@gmail.com>
2026-06-05 15:39:41 +0400

    payslip list

--- payslip_list/models/ht_payslip.py
+++ payslip_list/models/ht_payslip.py
@@ -4,93 +4,86 @@ from odoo import models, fields, api
 class HrPayslip(models.Model):
     _inherit = 'hr.payslip'
 
-    line_basic = fields.Float(
-        string='ძირითადი ხელფასი',
+    line_contract_wage = fields.Float(
+        string='საშტატო ხელფასი',
+        compute='_compute_contract_wage',
+        store=True,
+        group_operator='sum'
+    )
+    line_salary = fields.Float(
+        string='ხელფასი',
         compute='_compute_lines',
         store=True,
         group_operator='sum'
     )
-    line_db = fields.Float(
-        string='დასაბეგრი ხელფასი',
+    line_leave = fields.Float(
+        string='შვებულება',
         compute='_compute_lines',
         store=True,
         group_operator='sum'
     )
-    line_mob = fields.Float(
-        string='ზეგანაკვეთური',
+    line_bulletin = fields.Float(
+        string='ბიულეტენი',
         compute='_compute_lines',
         store=True,
         group_operator='sum'
     )
-    line_trip = fields.Float(
-        string='მივლინების დაკავება',
-        compute='_compute_lines',
-        store=True,
-        group_operator='sum'
-    )
-    line_prem = fields.Float(
+    line_bonus = fields.Float(
         string='პრემია',
         compute='_compute_lines',
         store=True,
         group_operator='sum'
     )
-    line_qvelmoqmedeba = fields.Float(
-        string='ქველმოქმედება',
+    line_help = fields.Float(
+        string='დახმარება',
         compute='_compute_lines',
         store=True,
         group_operator='sum'
     )
-    line_fitpass = fields.Float(
-        string='ფიტპასი',
+    line_overtime = fields.Float(
+        string='ზეგანაკვეთური ხელფასი',
         compute='_compute_lines',
         store=True,
         group_operator='sum'
     )
-    line_sapcomp = fields.Float(
-        string='კომპანიის 2 %',
+    line_letter_plus = fields.Float(
+        string='წერილი +',
         compute='_compute_lines',
         store=True,
         group_operator='sum'
     )
-    line_sap = fields.Float(
-        string='საპენსიო 2 %',
+    line_letter_minus = fields.Float(
+        string='წერილი -',
         compute='_compute_lines',
         store=True,
         group_operator='sum'
     )
     line_gross = fields.Float(
-        string='დარიცხული ხელფასი',
+        string='სულ დარიცხული',
         compute='_compute_lines',
         store=True,
         group_operator='sum'
     )
-    line_income = fields.Float(
+    line_pension = fields.Float(
+        string='საპენსიო ფონდი',
+        compute='_compute_lines',
+        store=True,
+        group_operator='sum'
+    )
+    line_taxable = fields.Float(
+        string='დასაბეგრი დარიცხვა',
+        compute='_compute_lines',
+        store=True,
+        group_operator='sum'
+    )
+    line_tax_exemption = fields.Float(
+        string='საშემოსავლო შეღავათი',
+        compute='_compute_lines',
+        store=True,
+        group_operator='sum'
+    )
+    line_income_tax = fields.Float(
         string='საშემოსავლო',
-        compute='_compute_lines',
-        store=True,
-        group_operator='sum'
-    )
-
-    line_assig_salary = fields.Float(
-        string='ანაზღაურების დანიშვნა',
-        compute='_compute_lines',
-        store=True,
-        group_operator='sum'
-    )
-    line_profkavsh= fields.Float(
-        string='პროფკავშირი მერია',
-        compute='_compute_lines',
-        store=True,
-        group_operator='sum'
-    )
-    line_profkavsh_1 = fields.Float(
-        string='პროფკავშირი 1 %',
-        compute='_compute_lines',
-        store=True,
-        group_operator='sum'
-    )
-    line_fondi = fields.Float(
-        string='სოლიდარობის ფონდი',
         compute='_compute_lines',
         store=True,
         group_operator='sum'
@@ -101,86 +94,110 @@ class HrPayslip(models.Model):
         store=True,
         group_operator='sum'
     )
-    line_agsr = fields.Float(
-        string='სააღსრულებო ფურცელი',
+    line_charity = fields.Float(
+        string='ქველმოქმედება',
         compute='_compute_lines',
         store=True,
         group_operator='sum'
     )
-    line_pension_4 = fields.Float(
-        string='საპენსიო 4 %',
+    line_execution = fields.Float(
+        string='საარსრულებო ფურცელი',
         compute='_compute_lines',
         store=True,
         group_operator='sum'
     )
-    line_compsap = fields.Float(
-        string='დაზღვევაზე კომპანიის საპენსიო',
+    line_alimony = fields.Float(
+        string='ალიმენტი',
         compute='_compute_lines',
         store=True,
         group_operator='sum'
     )
-    line_tansap = fields.Float(
-        string='დაზღვევაზე თანამშრომლის საპენსიო',
+    line_insurance = fields.Float(
+        string='დაზღვევა',
         compute='_compute_lines',
         store=True,
         group_operator='sum'
     )
-    line_compdazg = fields.Float(
-        string='სტანდარტ დაზღვევა',
+    line_fitpass = fields.Float(
+        string='ფიტპასი',
+        compute='_compute_lines',
+        store=True,
+        group_operator='sum'
+    )
+    line_solidarity = fields.Float(
+        string='სოლიდარობის ფონდი',
+        compute='_compute_lines',
+        store=True,
+        group_operator='sum'
+    )
+    line_prof_union = fields.Float(
+        string='პროფკავშირი',
+        compute='_compute_lines',
+        store=True,
+        group_operator='sum'
+    )
+    line_prof_union_meria = fields.Float(
+        string='პროფკავშირი მერია',
+        compute='_compute_lines',
+        store=True,
+        group_operator='sum'
+    )
+    line_fine = fields.Float(
+        string='ჯარიმა',
+        compute='_compute_lines',
+        store=True,
+        group_operator='sum'
+    )
+    line_other_deductions = fields.Float(
+        string='სხვა დაკავებები',
+        compute='_compute_lines',
+        store=True,
+        group_operator='sum'
+    )
+    line_transfer_salary = fields.Float(
+        string='გადასარიცხი ხელფასი',
         compute='_compute_lines',
         store=True,
         group_operator='sum'
     )
 
-    line_avansi = fields.Float(
-        string='ავანსად გაცემული ხელფასი/შვებულება',
-        compute='_compute_lines',
-        store=True,
-        group_operator='sum'
-    )
-    line_premiapr = fields.Float(
-        string='პრემია %',
-        compute='_compute_lines',
-        store=True,
-        group_operator='sum'
-    )
-    line_biulet = fields.Float(
-        string='ბიულეტენი',
-        compute='_compute_lines',
-        store=True,
-        group_operator='sum'
-    )
+    @api.depends('contract_id', 'contract_id.wage')
+    def _compute_contract_wage(self):
+        for slip in self:
+            slip.line_contract_wage = slip.contract_id.wage if slip.contract_id else 0.0
 
     @api.depends('line_ids.total', 'line_ids.salary_rule_id.name')
     def _compute_lines(self):
         code_to_field = {
-            'ძირითადი ხელფასი': 'line_basic', # +
-            'დასაბეგრი ხელფასი': 'line_db', #+
-            'ზეგანაკვეთური': 'line_mob', #+
-            # 'მივლინების დაკავება': 'line_trip',
-            # 'პრემია': 'line_prem',
-            'ქველმოქმედება': 'line_qvelmoqmedeba',  # +
-            'ფიტპასი': 'line_fitpass', # +
-            'საპენსიო 2 %': 'line_sap',#+
-            'კომპანიის 2 %': 'line_sapcomp',#+
-            'დარიცხული ხელფასი': 'line_gross', #+
-            'საშემოსავლო': 'line_income', # +
-            # 'პრემია %': 'line_premiapr',
-            'პროფკავშირი მერია': 'line_profkavsh', # +
-            'პროფკავშირი 1 %': 'line_profkavsh_1', # +
-            'სოლიდარობის ფონდი': 'line_fondi', # +
-            'ხელზე ასაღები ხელფასი': 'line_net', # +
-            'ბიულეტენი' : 'line_biulet', # daamate +
-            'სააღსრულებო ფურცელი': 'line_agsr', #gros moashore da daamate +
-            # 'საპენსიო 4%': 'line_pension_4', # დაამატე ფილდი
-            # 'დაზღვევაზე კომპანიის საპენსიო' : 'line_compsap', # დაამატე ფილდი
-            # 'დაზღვევაზე თანამშრომლის საპენსიო':'line_tansap', # დაამატე ფილდი
-            'სტანდარტ დაზღვევა' : 'line_compdazg',  # დაამატე ფილდი +
-            'შვებულება' : 'line_avansi' , #daamate +
+            'ხელფასი': 'line_salary',
+            'შვებულება': 'line_leave',
+            'ბიულეტენი': 'line_bulletin',
+            'პრემია': 'line_bonus',
+            'დახმარება': 'line_help',
+            'ზეგანაკვეთური ხელფასი': 'line_overtime',
+            'წერილი +': 'line_letter_plus',
+            'წერილი -': 'line_letter_minus',
+            'სულ დარიცხული': 'line_gross',
+            'საპენსიო ფონდი': 'line_pension',
+            'დასაბეგრი დარიცხვა': 'line_taxable',
+            'საშემოსავლო შეღავათი': 'line_tax_exemption',
+            'საშემოსავლო': 'line_income_tax',
+            'ხელზე ასაღები ხელფასი': 'line_net',
+            'ქველმოქმედება': 'line_charity',
+            'საარსრულებო ფურცელი': 'line_execution',
+            'ალიმენტი': 'line_alimony',
+            'დაზღვევა': 'line_insurance',
+            'ფიტპასი': 'line_fitpass',
+            'სოლიდარობის ფონდი': 'line_solidarity',
+            'პროფკავშირი': 'line_prof_union',
+            'პროფკავშირი მერია': 'line_prof_union_meria',
+            'ჯარიმა': 'line_fine',
+            'სხვა დაკავებები': 'line_other_deductions',
+            'გადასარიცხი ხელფასი': 'line_transfer_salary',
         }
 
         for slip in self:
-            code_map = {l.salary_rule_id.name : l.total for l in slip.line_ids}
+            code_map = {l.salary_rule_id.name: l.total for l in slip.line_ids}
 
             for field_name in code_to_field.values():
                 setattr(slip, field_name, 0.0)
