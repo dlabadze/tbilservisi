b1f69937851bf73e2f476fad16340671cd66db71
lizaKN <gvasalializa3@gmail.com>
2026-06-08 15:20:34 +0400

    hr synch dep adn job

--- approval_vacancy_button/models/approval_request.py
+++ approval_vacancy_button/models/approval_request.py
@@ -1,4 +1,5 @@
 from odoo import models, fields, api
+from datetime import timedelta
 
 PERSONAL_APPLICATION_CATEGORIES = [10, 11, 12, 13,44,25,45,46,47,48,49]
 
@@ -13,6 +14,21 @@ class ApprovalRequest(models.Model):
         store=False,
     )
 
+    release_date = fields.Date(
+        string="გათავისუფლების თარიღი",
+        compute="_compute_release_date",
+        store=True,
+        readonly=False,
+    )
+
+    @api.depends('brdzaneba_end_date', 'category_id')
+    def _compute_release_date(self):
+        for rec in self:
+            if rec.category_id and rec.category_id.id in [24, 25, 26, 27] and rec.brdzaneba_end_date:
+                rec.release_date = rec.brdzaneba_end_date - timedelta(days=1)
+            else:
+                rec.release_date = False
+
     @api.onchange('category_id', 'brdzaneba_employee_id', 'brdzaneba_date')
     def _onchange_brdzaneba_safudzveli(self):
         for rec in self:
@@ -24,3 +40,9 @@ class ApprovalRequest(models.Model):
                 if rec.brdzaneba_date:
                     parts.append(rec.brdzaneba_date.strftime('%d.%m.%Y'))
                 rec.brdzaneba_safudzveli = ', '.join(parts)
+
+    @api.onchange('category_id')
+    def _onchange_brdzaneba_shtati(self):
+        for rec in self:
+            if rec.category_id and rec.category_id.id == 11:
+                rec.brdzaneba_shtati = 'შტატგარეშე'
---
+++ hr_employee_sync_contract/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': 'HR Employee Sync Contract Dep And Job',
+    'version': '1.0',
+    'category': 'Human Resources',
+    'depends': ['hr', 'hr_contract'],
+    'data': [
+        'data/server_action.xml',
+    ],
+    'installable': True,
+    'application': False,
+    'license': 'LGPL-3',
+}
