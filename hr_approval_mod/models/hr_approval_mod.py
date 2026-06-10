c1398c2d5c320763cc79f575d216d801d3055251
lizaKN <gvasalializa3@gmail.com>
2026-06-10 14:38:17 +0400

    ss

--- hr_approval_mod/models/hr_approval_mod.py
+++ hr_approval_mod/models/hr_approval_mod.py
@@ -24,7 +24,7 @@ class ApprovalRequest(models.Model):
     brdzaneba_current_department_id = fields.Many2one('hr.department', related="brdzaneba_employee_id.department_id", string="მიმდინარე სტრუქტურული ერთეული")
     brdzaneba_current_job_id = fields.Many2one('hr.job', related="brdzaneba_employee_id.job_id", string="მიმდინარე თანამდებობა")
     brdzaneba_start_date = fields.Date(string="დაწყების თარიღი")
-    brdzaneba_end_date = fields.Date(string="დასრულების თარიღი",  compute="_compute_end_date", readonly=False)
+    brdzaneba_end_date = fields.Date(string="დასრულების თარიღი")
     brdzaneba_department_id = fields.Many2one('hr.department', string="სტრუქტურული ერთეული")
     brdzaneba_job_id = fields.Many2one('hr.job', string="თანამდებობა", domain="[('department_id', '=', brdzaneba_department_id)]")
     brdzaneba_shtati = fields.Selection([
@@ -41,8 +41,8 @@ class ApprovalRequest(models.Model):
         readonly=False,
     )
 
-    @api.depends('release_date', 'category_id')
-    def _compute_end_date(self):
+    @api.onchange('release_date', 'category_id')
+    def _onchange_end_date(self):
         for rec in self:
             if rec.category_id and rec.category_id.id in [24, 25, 26, 27, 29, 30, 32] and rec.release_date:
                 rec.brdzaneba_end_date = rec.release_date - timedelta(days=1)
