560f94e6446cf791bbc0e7bbc62a299572c0b5a6
lizaKN <gvasalializa3@gmail.com>
2026-06-12 18:54:56 +0400

    studio type shii

---
+++ studio_fields/__manifest__.py
@@ -0,0 +1,20 @@
+{
+    'name': 'Studio Fields',
+    'version': '18.0.1.0.0',
+    'summary': 'Studio Fields',
+    'depends': [
+        'hr',
+        'web_studio',
+        'hr_attendance',
+        'hr_holidays',
+        'approvals',
+        'hr_approval_mod',
+        'employee_vacancy_info',
+    ],
+    'data': [
+
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
---
+++ studio_fields/models/approval_request.py
@@ -0,0 +1,5 @@
+from odoo import api, fields, models
+
+class ApprovalRequest(models.Model):
+    _inherit = 'approval.request'
+
