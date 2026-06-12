3ae3bca9b6dd96a1f38b45a474f60d94e5b25cdc
Luka Goderdzishvili <lukagoderdzishvili4@gmail.com>
2026-06-12 19:28:15 +0400

    fields_invisible

---
+++ fields_invisible/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    'name': 'Fields invisible tbilservisi',
+    'version': '1.0',
+    'depends': ['base', 'hr_recruitment'],
+    'author': 'odoo',
+    'description': 'Invisible fields',
+    'data': [
+        'views/hr_job_views.xml',
+    ],
+}
