1790f14073aa53da0751ee2e5139908a285e09b1
Luka Goderdzishvili <lukagoderdzishvili4@gmail.com>
2026-06-15 15:44:00 +0400

    journal_id_readonly

---
+++ journal_id_readonly/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    "name": "Journal ID Readonly",
+    "version": "1.0",
+    "depends": ["account"],
+    "data": [
+        "views/account_move_views.xml",
+    ],
+    "installable": True,
+    "application": False,
+    "license": "LGPL-3",
+}
