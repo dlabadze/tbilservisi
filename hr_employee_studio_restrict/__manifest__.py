80771f098682f07ae2f86f7208f8fc3d634da701
lizaKN <gvasalializa3@gmail.com>
2026-06-16 16:38:58 +0400

    hr security views

--- hr_employee_studio_restrict/__manifest__.py
+++ hr_employee_studio_restrict/__manifest__.py
@@ -3,9 +3,10 @@
     'version': '18.0.1.0.0',
     'category': 'Human Resources',
     'license': 'LGPL-3',
-    'depends': ['hr', 'web_studio'],
+    'depends': ['hr', 'web_studio','partner_employee_pen', 'employee_field_tracking'],
     'data': [
         'security/hr_employee_security.xml',
+        'security/ir.model.access.csv',
         'views/hr_employee_views.xml',
     ],
     'installable': True,
