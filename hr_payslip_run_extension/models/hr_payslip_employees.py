9bb430feace57851d6a214405fe439f616c3ef3d
Luka Goderdzishvili <lukagoderdzishvili4@gmail.com>
2026-06-24 13:32:28 +0400

    hr payslip run ext

--- hr_payslip_run_extension/models/hr_payslip_employees.py
+++ hr_payslip_run_extension/models/hr_payslip_employees.py
@@ -1,13 +1,12 @@
 # -*- coding: utf-8 -*-
 from odoo import api, fields, models
-from odoo.api import NewId
 from odoo.osv import expression
 
 
 class HrPayslipEmployees(models.TransientModel):
     _inherit = 'hr.payslip.employees'
 
-    danamatebi_id = fields.Many2one(
+    danamatebi_ids = fields.Many2many(
         'zeganakveturi_saati',
         string='დანამატები',
     )
@@ -17,18 +16,18 @@ class HrPayslipEmployees(models.TransientModel):
         'department_id',
         'structure_type_id',
         'job_id',
-        'danamatebi_id',
+        'danamatebi_ids',
     )
     def _compute_employee_ids(self):
         super()._compute_employee_ids()
 
-    @api.onchange('danamatebi_id')
-    def _onchange_danamatebi_id(self):
+    @api.onchange('danamatebi_ids')
+    def _onchange_danamatebi_ids(self):
         self._compute_employee_ids()
 
     def get_employees_domain(self):
         domain = super().get_employees_domain()
-        if self.danamatebi_id and not isinstance(self.danamatebi_id.id, NewId):
-            allowed_ids = self.danamatebi_id.zeganakveturi_saati_line_ids.employee_id.ids
+        if self.danamatebi_ids.ids:
+            allowed_ids = self.danamatebi_ids.mapped('zeganakveturi_saati_line_ids.employee_id').ids
             domain = expression.AND([domain, [('id', 'in', allowed_ids)]])
         return domain
