b3dab777b6225663b5f7c3b5ef8249b8f30fef3d
Luka Goderdzishvili <lukagoderdzishvili4@gmail.com>
2026-06-26 17:32:58 +0400

    salary management excel report

---
+++ salary_management_excel_report/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    'name': 'Salary Management Excel Report',
+    'version': '1.0',
+    'depends': ['salary_management'],
+    'data': [
+        # 'views/salary_import_views.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+}
---
+++ salary_management_excel_report/models/salary_import.py
@@ -0,0 +1,12 @@
+from odoo import fields, models, api, _
+from odoo.exceptions import UserError
+
+
+class SalaryImport(models.Model):
+    _inherit = 'salary.import'
+
+
+
+    def action_generate_excel(self):
+        lines = self.env['salary.import.line'].search([('import_id', 'in', self.ids)])
+        raise UserError(lines)
