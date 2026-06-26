8a5e8a8f425e94ce1cdbc35c84687a7386b0da1f
Luka Goderdzishvili <lukagoderdzishvili4@gmail.com>
2026-06-26 17:42:28 +0400

    rameeeee

--- salary_management_excel_report/__manifest__.py
+++ salary_management_excel_report/__manifest__.py
@@ -1,10 +1,14 @@
 {
     'name': 'Salary Management Excel Report',
     'version': '1.0',
-    'depends': ['salary_management'],
+    'depends': [
+        'salary_management',
+    ],
     'data': [
-        # 'views/salary_import_views.xml',
+        'views/salary_import_views.xml',
     ],
     'installable': True,
     'auto_install': False,
+    'application': False,
+    'license': 'LGPL-3',
 }
--- salary_management_excel_report/models/salary_import.py
+++ salary_management_excel_report/models/salary_import.py
@@ -6,7 +6,6 @@ class SalaryImport(models.Model):
     _inherit = 'salary.import'
 
 
-
     def action_generate_excel(self):
         lines = self.env['salary.import.line'].search([('import_id', 'in', self.ids)])
         raise UserError(lines)
