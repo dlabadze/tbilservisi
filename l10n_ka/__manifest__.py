39cf370ab5ee2b89f980121cde12b0ad8f9acf7f
Giorgi Olqiashvilli <giorgiolqiashvili2004@gmail.com>
2026-06-29 23:08:17 +0400

    Georgian language

--- l10n_ka/__manifest__.py
+++ l10n_ka/__manifest__.py
@@ -8,9 +8,9 @@
     'website': '',
     'license': 'LGPL-3',
     'depends': [
-        'web', 'mail', 'documents_spreadsheet', 'spreadsheet_edition',
+        'web', 'mail', 'calendar', 'documents', 'documents_spreadsheet', 'spreadsheet_edition',
         'approvals', 'sign', 'base_import',
-        'hr_contract', 'hr_holidays', 'hr_maintenance',
+        'hr_contract', 'hr_holidays', 'hr_attendance', 'hr_maintenance', 'hr_recruitment',
         'hr_work_entry', 'hr_skills', 'hr_contract_sign', 'hr_payroll',
         'hr_work_entry_contract', 'hr_work_entry_contract_enterprise', 'hr_payroll_holidays',
         'web_gantt', 'contacts'
