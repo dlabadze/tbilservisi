0479f4f9907ed809bcf44c8799259b24ac4f5b56
lizaKN <gvasalializa3@gmail.com>
2026-06-30 19:04:35 +0400

    no no tracking

--- employee_field_tracking/models/hr_employee.py
+++ employee_field_tracking/models/hr_employee.py
@@ -4,22 +4,22 @@ from odoo import models, fields, api
 class HrEmployeeTracking(models.Model):
     _inherit = 'hr.employee'
 
-    mobile_phone = fields.Char(tracking=False)
-    x_studio_homephone = fields.Char(tracking=False)
-    x_studio_pension = fields.Boolean(tracking=False)
-    x_studio_insurance = fields.Float(tracking=False)
-    x_studio_fitpass = fields.Float(tracking=False)
-    x_studio_fondi_solidaroba = fields.Float(tracking=False)
-    x_studio_charity = fields.Float(tracking=False)
-    x_studio_profkav = fields.Float(tracking=False)
-    x_studio_profm = fields.Float(tracking=False)
-    x_studio_alimenti = fields.Float(tracking=False)
-    x_studio_alimentiper = fields.Float(tracking=False)
-    x_studio_agsruleba = fields.Float(tracking=False)
-    x_studio_agsrulebaper = fields.Float(tracking=False)
-    x_studio_shegavati = fields.Float(tracking=False)
-    x_studio_start_date_1 = fields.Date(tracking=False)
-    bank_account_id = fields.Many2one('res.partner.bank', tracking=False)
+    mobile_phone = fields.Char(tracking=True)
+    x_studio_homephone = fields.Char(tracking=True)
+    x_studio_pension = fields.Boolean(tracking=True)
+    x_studio_insurance = fields.Float(tracking=True)
+    x_studio_fitpass = fields.Float(tracking=True)
+    x_studio_fondi_solidaroba = fields.Float(tracking=True)
+    x_studio_charity = fields.Float(tracking=True)
+    x_studio_profkav = fields.Float(tracking=True)
+    x_studio_profm = fields.Float(tracking=True)
+    x_studio_alimenti = fields.Float(tracking=True)
+    x_studio_alimentiper = fields.Float(tracking=True)
+    x_studio_agsruleba = fields.Float(tracking=True)
+    x_studio_agsrulebaper = fields.Float(tracking=True)
+    x_studio_shegavati = fields.Float(tracking=True)
+    x_studio_start_date_1 = fields.Date(tracking=True)
+    bank_account_id = fields.Many2one('res.partner.bank', tracking=True)
 
 
 
